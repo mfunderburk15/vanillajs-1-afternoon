ae0cfa8a240d77070d5e05fcf56ace3478db5d2d
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-08-11 17:13:19 -0500

    finished step3

--- Part2/index.js
+++ Part2/index.js
@@ -3,3 +3,8 @@ const colorInput = document.getElementById("colorInput")
 
 console.log(idInput)
 console.log(colorInput)
+
+const setCard = () => {
+    const card = document.getElementById(idInput.value);
+    console.log(card);
+}
