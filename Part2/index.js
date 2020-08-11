c07e52b80ad7a56871dab6ac9bbec2349bc783e0
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-08-11 17:28:24 -0500

    finished base parts, black diamonds still need to be worked on

--- Part2/index.js
+++ Part2/index.js
@@ -1,10 +1,8 @@
 const idInput = document.getElementById("idInput")
 const colorInput = document.getElementById("colorInput")
 
-console.log(idInput)
-console.log(colorInput)
 
-const setCard = () => {
+function setCard() {
     const card = document.getElementById(idInput.value);
-    console.log(card);
+    card.style.color = colorInput.value;
 }
